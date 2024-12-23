d839e507a4d8ed4f0862ec5964eabdb6af943434
Sumit <panchalsumite2logy@gmail.com>
2024-12-23 18:16:48 +0530

    ui update

--- lib/view/login/LoginPage.dart
+++ lib/view/login/LoginPage.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:fluttertoast/fluttertoast.dart'; // Import fluttertoast
+import 'package:get/get.dart';
 import 'package:practice_flutter/routes/routes_name.dart';
 
 import 'controllers/login_controller.dart';
@@ -13,26 +13,43 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final emailController = TextEditingController();
     final passwordController = TextEditingController();
+    final isPasswordVisible = false.obs; // Observable for password visibility
     final controller = Get.find<LoginController>();
 
+    // Fetch screen dimensions for responsive design
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _header(),
-            const SizedBox(height: 50),
-            Obx(() {
-              return controller.isLoading.value
-                  ? const Center(child: CircularProgressIndicator())
-                  : _inputField(context, emailController, passwordController);
-            }),
-            const SizedBox(height: 30),
-            _forgotPassword(context),
-            _signup(context),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.08, // 8% of screen width
+            vertical: screenHeight * 0.02, // 2% of screen height
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: screenHeight * 0.1), // 10% of screen height
+              _header(),
+              SizedBox(height: screenHeight * 0.05), // 5% of screen height
+              Obx(() {
+                return controller.isLoading.value
+                    ? const Center(child: CircularProgressIndicator())
+                    : _inputField(
+                        context,
+                        emailController,
+                        passwordController,
+                        screenWidth,
+                        isPasswordVisible,
+                      );
+              }),
+              SizedBox(height: screenHeight * 0.03), // 3% of screen height
+              _forgotPassword(context),
+              _signup(context),
+            ],
+          ),
         ),
       ),
     );
@@ -43,14 +60,22 @@ class LoginPage extends StatelessWidget {
       children: [
         Text(
           "Sign In",
-          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
   }
 
-  _inputField(BuildContext context, TextEditingController emailController,
-      TextEditingController passwordController) {
+  _inputField(
+    BuildContext context,
+    TextEditingController emailController,
+    TextEditingController passwordController,
+    double screenWidth,
+    RxBool isPasswordVisible,
+  ) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -59,36 +84,48 @@ class LoginPage extends StatelessWidget {
           decoration: InputDecoration(
             hintText: "Email",
             border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(18),
-                borderSide: BorderSide.none),
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
             fillColor: Colors.blue.withOpacity(0.1),
             filled: true,
             prefixIcon: const Icon(Icons.email),
           ),
         ),
-        const SizedBox(height: 26),
-        TextField(
-          controller: passwordController,
-          decoration: InputDecoration(
-            hintText: "Password",
-            border: OutlineInputBorder(
+        SizedBox(height: screenWidth * 0.05), // 5% of screen width
+        Obx(() {
+          return TextField(
+            controller: passwordController,
+            obscureText: !isPasswordVisible.value, // Hide or show password
+            decoration: InputDecoration(
+              hintText: "Password",
+              border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(18),
-                borderSide: BorderSide.none),
-            fillColor: Colors.blue.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.lock),
-          ),
-          obscureText: true,
-        ),
-        const SizedBox(height: 20),
+                borderSide: BorderSide.none,
+              ),
+              fillColor: Colors.blue.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.lock),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  isPasswordVisible.value
+                      ? Icons.visibility
+                      : Icons.visibility_off,
+                ),
+                onPressed: () {
+                  isPasswordVisible.value = !isPasswordVisible.value;
+                },
+              ),
+            ),
+          );
+        }),
+        SizedBox(height: screenWidth * 0.04), // 4% of screen width
         ElevatedButton(
           onPressed: () {
             _loginWithFirebase(
                 context, emailController.text, passwordController.text);
           },
           style: ElevatedButton.styleFrom(
-            shape: const StadiumBorder(),
-            padding: const EdgeInsets.symmetric(vertical: 16),
             backgroundColor: Colors.blue,
           ),
           child: const Text(
@@ -131,27 +168,7 @@ class LoginPage extends StatelessWidget {
         gravity: ToastGravity.BOTTOM,
         timeInSecForIosWeb: 1,
       );
-      _showErrorDialog(context, message);
     }
-  }
-
-  // Function to show error dialog
-  void _showErrorDialog(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Error'),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
   }
 
   _forgotPassword(BuildContext context) {
